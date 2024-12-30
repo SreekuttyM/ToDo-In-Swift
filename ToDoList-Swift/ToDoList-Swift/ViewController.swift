a948996f094ee69829292b6945bb0ffa361e2a22
Sreekutty Maya <sreekuttystar@gmail.com>
2024-12-30 19:45:24 +0400

    Project SetUp Added

---
+++ ToDoList-Swift/ToDoList-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList-Swift
+//
+//  Created by Sreekutty Maya on 30/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
